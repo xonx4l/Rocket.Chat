0e60efe640cbab2561118fe7c4ec438794447fad
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2024-03-11 11:53:00 -0300

    Undo unrelated changes

--- apps/meteor/app/lib/server/methods/insertOrUpdateUser.ts
+++ apps/meteor/app/lib/server/methods/insertOrUpdateUser.ts
@@ -4,7 +4,6 @@ import { Meteor } from 'meteor/meteor';
 
 import { twoFactorRequired } from '../../../2fa/server/twoFactorRequired';
 import { saveUser } from '../functions/saveUser';
-import type { ISaveUserDataParams } from '../functions/saveUser';
 import { methodDeprecationLogger } from '../lib/deprecationWarningLogger';
 
 declare module '@rocket.chat/ui-contexts' {
@@ -19,14 +18,13 @@ Meteor.methods<ServerMethods>({
 		methodDeprecationLogger.method('insertOrUpdateUser', '8.0.0');
 
 		check(userData, Object);
-		const userId = Meteor.userId();
 
-		if (!userId) {
+		if (!Meteor.userId()) {
 			throw new Meteor.Error('error-invalid-user', 'Invalid user', {
 				method: 'insertOrUpdateUser',
 			});
 		}
 
-		return saveUser(userId, userData as ISaveUserDataParams);
+		return saveUser(Meteor.userId(), userData);
 	}),
 });
