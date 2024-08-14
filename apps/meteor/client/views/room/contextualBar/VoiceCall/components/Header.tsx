a2e822139d4c233dce9b58192288deb94be8c204
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2024-08-14 13:45:38 -0300

    feat: added SettingsButton to Header

--- apps/meteor/client/views/room/contextualBar/VoiceCall/components/Header.tsx
+++ apps/meteor/client/views/room/contextualBar/VoiceCall/components/Header.tsx
@@ -2,17 +2,26 @@ import { Box, IconButton } from '@rocket.chat/fuselage';
 import type { ReactNode } from 'react';
 import React from 'react';
 
+import VoiceCallSettingsButton from './CallSettingsButton';
+
 type VoiceCallHeaderProps = {
 	children: ReactNode;
+	hideSettings?: boolean;
 	onClose?: () => void;
 };
 
-const VoiceCallHeader = ({ children, onClose }: VoiceCallHeaderProps) => {
+const VoiceCallHeader = ({ children, hideSettings, onClose }: VoiceCallHeaderProps) => {
 	return (
 		<Box is='header' p={12} pbe={4} display='flex' alignItems='center' justifyContent='space-between'>
 			{children && (
 				<Box is='h3' id='voiceCallPopupTitle' color='font-titles-labels' fontScale='p2' fontWeight='700'>
 					{children}
+				</Box>
+			)}
+
+			{!hideSettings && (
+				<Box mis={8}>
+					<VoiceCallSettingsButton mini />
 				</Box>
 			)}
 
